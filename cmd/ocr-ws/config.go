5dce8b5e9a5f306f163a2e6552a387ed65ab8250
Jason Jordan <jlj5aj@virginia.edu>
2018-12-14 12:53:12 -0500

    add tracksys api url support

--- cmd/ocr-ws/config.go
+++ cmd/ocr-ws/config.go
@@ -23,51 +23,54 @@ type configBoolItem struct {
 }
 
 type configData struct {
-	listenPort          configStringItem
-	dbHost              configStringItem
-	dbName              configStringItem
-	dbUser              configStringItem
-	dbPass              configStringItem
-	dbAllowOldPasswords configBoolItem
-	jp2kDir             configStringItem
-	archiveDir          configStringItem
-	storageDir          configStringItem
-	scriptDir           configStringItem
-	allowUnpublished    configBoolItem
-	iiifUrlTemplate     configStringItem
-	workerType          configStringItem
-	workerCount         configStringItem
-	useHttps            configBoolItem
-	sslCrt              configStringItem
-	sslKey              configStringItem
-
-	awsAccessKeyId        configStringItem
-	awsSecretAccessKey    configStringItem
-	awsRegion             configStringItem
-	awsSwfDomain          configStringItem
-	awsSwfTaskList        configStringItem
-	awsSwfWorkflowType    configStringItem
-	awsSwfWorkflowVersion configStringItem
-	awsSwfWorkflowTimeout configStringItem
-	awsSwfDecisionTimeout configStringItem
-	awsLambdaFunction     configStringItem
-	awsLambdaRole         configStringItem
+	listenPort               configStringItem
+	jp2kDir                  configStringItem
+	archiveDir               configStringItem
+	storageDir               configStringItem
+	scriptDir                configStringItem
+	iiifUrlTemplate          configStringItem
+	useHttps                 configBoolItem
+	sslCrt                   configStringItem
+	sslKey                   configStringItem
+	tsDBHost                 configStringItem
+	tsDBName                 configStringItem
+	tsDBUser                 configStringItem
+	tsDBPass                 configStringItem
+	tsDBAllowOldPasswords    configBoolItem
+	allowUnpublished         configBoolItem
+	workerType               configStringItem
+	workerCount              configStringItem
+	tsAPIPidUrlTemplate      configStringItem
+	tsAPIManifestUrlTemplate configStringItem
+	awsAccessKeyId           configStringItem
+	awsSecretAccessKey       configStringItem
+	awsRegion                configStringItem
+	awsSwfDomain             configStringItem
+	awsSwfTaskList           configStringItem
+	awsSwfWorkflowType       configStringItem
+	awsSwfWorkflowVersion    configStringItem
+	awsSwfWorkflowTimeout    configStringItem
+	awsSwfDecisionTimeout    configStringItem
+	awsLambdaFunction        configStringItem
+	awsLambdaRole            configStringItem
 }
 
 var config configData
 
 func init() {
 	config.listenPort = configStringItem{value: "", configItem: configItem{flag: "l", env: "OCRWS_LISTEN_PORT", desc: "listen port"}}
-	config.dbHost = configStringItem{value: "", configItem: configItem{flag: "h", env: "OCRWS_DB_HOST", desc: "database host"}}
-	config.dbName = configStringItem{value: "", configItem: configItem{flag: "n", env: "OCRWS_DB_NAME", desc: "database name"}}
-	config.dbUser = configStringItem{value: "", configItem: configItem{flag: "u", env: "OCRWS_DB_USER", desc: "database user"}}
-	config.dbPass = configStringItem{value: "", configItem: configItem{flag: "p", env: "OCRWS_DB_PASS", desc: "database password"}}
-	config.dbAllowOldPasswords = configBoolItem{value: false, configItem: configItem{flag: "o", env: "OCRWS_DB_ALLOW_OLD_PASSWORDS", desc: "allow old database passwords"}}
+	config.tsDBHost = configStringItem{value: "", configItem: configItem{flag: "h", env: "OCRWS_DB_HOST", desc: "database host"}}
+	config.tsDBName = configStringItem{value: "", configItem: configItem{flag: "n", env: "OCRWS_DB_NAME", desc: "database name"}}
+	config.tsDBUser = configStringItem{value: "", configItem: configItem{flag: "u", env: "OCRWS_DB_USER", desc: "database user"}}
+	config.tsDBPass = configStringItem{value: "", configItem: configItem{flag: "p", env: "OCRWS_DB_PASS", desc: "database password"}}
+	config.tsDBAllowOldPasswords = configBoolItem{value: false, configItem: configItem{flag: "o", env: "OCRWS_DB_ALLOW_OLD_PASSWORDS", desc: "allow old database passwords"}}
 	config.jp2kDir = configStringItem{value: "", configItem: configItem{flag: "j", env: "OCRWS_JP2K_DIR", desc: "jp2k directory"}}
 	config.archiveDir = configStringItem{value: "", configItem: configItem{flag: "m", env: "OCRWS_ARCHIVE_DIR", desc: "archival tif mount directory"}}
 	config.storageDir = configStringItem{value: "", configItem: configItem{flag: "t", env: "OCRWS_OCR_STORAGE_DIR", desc: "ocr storage directory"}}
 	config.scriptDir = configStringItem{value: "", configItem: configItem{flag: "r", env: "OCRWS_SCRIPT_DIR", desc: "helper script directory"}}
 	config.allowUnpublished = configBoolItem{value: false, configItem: configItem{flag: "a", env: "OCRWS_ALLOW_UNPUBLISHED", desc: "allow unpublished"}}
+	config.tsAPIPidUrlTemplate = configStringItem{value: "", configItem: configItem{flag: "d", env: "OCRWS_TRACKSYS_API_PID_URL_TEMPLATE", desc: "tracksys /api/pid url template"}}
+	config.tsAPIManifestUrlTemplate = configStringItem{value: "", configItem: configItem{flag: "f", env: "OCRWS_TRACKSYS_API_MANIFEST_URL_TEMPLATE", desc: "tracksys /api/manifest url template"}}
 	config.iiifUrlTemplate = configStringItem{value: "", configItem: configItem{flag: "i", env: "OCRWS_IIIF_URL_TEMPLATE", desc: "iiif url template"}}
 	config.workerType = configStringItem{value: "", configItem: configItem{flag: "y", env: "OCRWS_WORKER_TYPE", desc: "worker type (tesseract/other APIs tbd)"}}
 	config.workerCount = configStringItem{value: "", configItem: configItem{flag: "x", env: "OCRWS_WORKER_COUNT", desc: "number of workers in worker pool"}}
@@ -115,16 +118,18 @@ func flagBoolVar(item *configBoolItem) {
 func getConfigValues() {
 	// get values from the command line first, falling back to environment variables
 	flagStringVar(&config.listenPort)
-	flagStringVar(&config.dbHost)
-	flagStringVar(&config.dbName)
-	flagStringVar(&config.dbUser)
-	flagStringVar(&config.dbPass)
-	flagBoolVar(&config.dbAllowOldPasswords)
+	flagStringVar(&config.tsDBHost)
+	flagStringVar(&config.tsDBName)
+	flagStringVar(&config.tsDBUser)
+	flagStringVar(&config.tsDBPass)
+	flagBoolVar(&config.tsDBAllowOldPasswords)
 	flagStringVar(&config.jp2kDir)
 	flagStringVar(&config.archiveDir)
 	flagStringVar(&config.storageDir)
 	flagStringVar(&config.scriptDir)
 	flagBoolVar(&config.allowUnpublished)
+	flagStringVar(&config.tsAPIPidUrlTemplate)
+	flagStringVar(&config.tsAPIManifestUrlTemplate)
 	flagStringVar(&config.iiifUrlTemplate)
 	flagStringVar(&config.workerType)
 	flagStringVar(&config.workerCount)
@@ -149,14 +154,16 @@ func getConfigValues() {
 	// die if any of them are not set
 	configOK := true
 	configOK = ensureConfigStringSet(&config.listenPort) && configOK
-	configOK = ensureConfigStringSet(&config.dbHost) && configOK
-	configOK = ensureConfigStringSet(&config.dbName) && configOK
-	configOK = ensureConfigStringSet(&config.dbUser) && configOK
-	configOK = ensureConfigStringSet(&config.dbPass) && configOK
+	configOK = ensureConfigStringSet(&config.tsDBHost) && configOK
+	configOK = ensureConfigStringSet(&config.tsDBName) && configOK
+	configOK = ensureConfigStringSet(&config.tsDBUser) && configOK
+	configOK = ensureConfigStringSet(&config.tsDBPass) && configOK
 	configOK = ensureConfigStringSet(&config.jp2kDir) && configOK
 	configOK = ensureConfigStringSet(&config.archiveDir) && configOK
 	configOK = ensureConfigStringSet(&config.storageDir) && configOK
 	configOK = ensureConfigStringSet(&config.scriptDir) && configOK
+	configOK = ensureConfigStringSet(&config.tsAPIPidUrlTemplate) && configOK
+	configOK = ensureConfigStringSet(&config.tsAPIManifestUrlTemplate) && configOK
 	configOK = ensureConfigStringSet(&config.iiifUrlTemplate) && configOK
 	configOK = ensureConfigStringSet(&config.workerType) && configOK
 	configOK = ensureConfigStringSet(&config.workerCount) && configOK
@@ -182,32 +189,34 @@ func getConfigValues() {
 		os.Exit(1)
 	}
 
-	logger.Printf("[CONFIG] listenPort            = [%s]", config.listenPort.value)
-	logger.Printf("[CONFIG] dbHost                = [%s]", config.dbHost.value)
-	logger.Printf("[CONFIG] dbName                = [%s]", config.dbName.value)
-	logger.Printf("[CONFIG] dbUser                = [%s]", config.dbUser.value)
-	logger.Printf("[CONFIG] dbPass                = [REDACTED]")
-	logger.Printf("[CONFIG] dbAllowOldPasswords   = [%s]", strconv.FormatBool(config.dbAllowOldPasswords.value))
-	logger.Printf("[CONFIG] jp2kDir               = [%s]", config.jp2kDir.value)
-	logger.Printf("[CONFIG] archiveDir            = [%s]", config.archiveDir.value)
-	logger.Printf("[CONFIG] storageDir            = [%s]", config.storageDir.value)
-	logger.Printf("[CONFIG] scriptDir             = [%s]", config.scriptDir.value)
-	logger.Printf("[CONFIG] allowUnpublished      = [%s]", strconv.FormatBool(config.allowUnpublished.value))
-	logger.Printf("[CONFIG] iiifUrlTemplate       = [%s]", config.iiifUrlTemplate.value)
-	logger.Printf("[CONFIG] workerType            = [%s]", config.workerType.value)
-	logger.Printf("[CONFIG] workerCount           = [%s]", config.workerCount.value)
-	logger.Printf("[CONFIG] useHttps              = [%s]", strconv.FormatBool(config.useHttps.value))
-	logger.Printf("[CONFIG] sslCrt                = [%s]", config.sslCrt.value)
-	logger.Printf("[CONFIG] sslKey                = [%s]", config.sslKey.value)
-	logger.Printf("[CONFIG] awsAccessKeyId        = [%s]", config.awsAccessKeyId.value)
-	logger.Printf("[CONFIG] awsSecretAccessKey    = [REDACTED]")
-	logger.Printf("[CONFIG] awsRegion             = [%s]", config.awsRegion.value)
-	logger.Printf("[CONFIG] awsSwfDomain          = [%s]", config.awsSwfDomain.value)
-	logger.Printf("[CONFIG] awsSwfTaskList        = [%s]", config.awsSwfTaskList.value)
-	logger.Printf("[CONFIG] awsSwfWorkflowType    = [%s]", config.awsSwfWorkflowType.value)
-	logger.Printf("[CONFIG] awsSwfWorkflowVersion = [%s]", config.awsSwfWorkflowVersion.value)
-	logger.Printf("[CONFIG] awsSwfWorkflowTimeout = [%s]", config.awsSwfWorkflowTimeout.value)
-	logger.Printf("[CONFIG] awsSwfDecisionTimeout = [%s]", config.awsSwfDecisionTimeout.value)
-	logger.Printf("[CONFIG] awsLambdaFunction     = [%s]", config.awsLambdaFunction.value)
-	logger.Printf("[CONFIG] awsLambdaRole         = [%s]", config.awsLambdaRole.value)
+	logger.Printf("[CONFIG] listenPort               = [%s]", config.listenPort.value)
+	logger.Printf("[CONFIG] tsDBHost                 = [%s]", config.tsDBHost.value)
+	logger.Printf("[CONFIG] tsDBName                 = [%s]", config.tsDBName.value)
+	logger.Printf("[CONFIG] tsDBUser                 = [%s]", config.tsDBUser.value)
+	logger.Printf("[CONFIG] tsDBPass                 = [REDACTED]")
+	logger.Printf("[CONFIG] tsDBAllowOldPasswords    = [%s]", strconv.FormatBool(config.tsDBAllowOldPasswords.value))
+	logger.Printf("[CONFIG] jp2kDir                  = [%s]", config.jp2kDir.value)
+	logger.Printf("[CONFIG] archiveDir               = [%s]", config.archiveDir.value)
+	logger.Printf("[CONFIG] storageDir               = [%s]", config.storageDir.value)
+	logger.Printf("[CONFIG] scriptDir                = [%s]", config.scriptDir.value)
+	logger.Printf("[CONFIG] allowUnpublished         = [%s]", strconv.FormatBool(config.allowUnpublished.value))
+	logger.Printf("[CONFIG] tsAPIPidUrlTemplate      = [%s]", config.tsAPIPidUrlTemplate.value)
+	logger.Printf("[CONFIG] tsAPIManifestUrlTemplate = [%s]", config.tsAPIManifestUrlTemplate.value)
+	logger.Printf("[CONFIG] iiifUrlTemplate          = [%s]", config.iiifUrlTemplate.value)
+	logger.Printf("[CONFIG] workerType               = [%s]", config.workerType.value)
+	logger.Printf("[CONFIG] workerCount              = [%s]", config.workerCount.value)
+	logger.Printf("[CONFIG] useHttps                 = [%s]", strconv.FormatBool(config.useHttps.value))
+	logger.Printf("[CONFIG] sslCrt                   = [%s]", config.sslCrt.value)
+	logger.Printf("[CONFIG] sslKey                   = [%s]", config.sslKey.value)
+	logger.Printf("[CONFIG] awsAccessKeyId           = [%s]", config.awsAccessKeyId.value)
+	logger.Printf("[CONFIG] awsSecretAccessKey       = [REDACTED]")
+	logger.Printf("[CONFIG] awsRegion                = [%s]", config.awsRegion.value)
+	logger.Printf("[CONFIG] awsSwfDomain             = [%s]", config.awsSwfDomain.value)
+	logger.Printf("[CONFIG] awsSwfTaskList           = [%s]", config.awsSwfTaskList.value)
+	logger.Printf("[CONFIG] awsSwfWorkflowType       = [%s]", config.awsSwfWorkflowType.value)
+	logger.Printf("[CONFIG] awsSwfWorkflowVersion    = [%s]", config.awsSwfWorkflowVersion.value)
+	logger.Printf("[CONFIG] awsSwfWorkflowTimeout    = [%s]", config.awsSwfWorkflowTimeout.value)
+	logger.Printf("[CONFIG] awsSwfDecisionTimeout    = [%s]", config.awsSwfDecisionTimeout.value)
+	logger.Printf("[CONFIG] awsLambdaFunction        = [%s]", config.awsLambdaFunction.value)
+	logger.Printf("[CONFIG] awsLambdaRole            = [%s]", config.awsLambdaRole.value)
 }
