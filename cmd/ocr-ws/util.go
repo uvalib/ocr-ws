0174bdd53339147598fc29c8deba49f7342b7216
Jason Jordan <jlj5aj@virginia.edu>
2018-12-18 12:28:55 -0500

    split out generic utility functions

---
+++ cmd/ocr-ws/util.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"bufio"
+	"errors"
+	"fmt"
+	"os"
+)
+
+func writeFileWithContents(filename, contents string) error {
+	f, err := os.OpenFile(filename, os.O_CREATE|os.O_RDWR, 0666)
+
+	if err != nil {
+		logger.Printf("Unable to open file: %s", err.Error())
+		return errors.New(fmt.Sprintf("Unable to open ocr file: [%s]", filename))
+	}
+
+	defer f.Close()
+
+	w := bufio.NewWriter(f)
+
+	if _, err = fmt.Fprintf(w, "%s", contents); err != nil {
+		logger.Printf("Unable to write file: %s", err.Error())
+		return errors.New(fmt.Sprintf("Unable to write ocr file: [%s]", filename))
+	}
+
+	w.Flush()
+
+	return nil
+}
+
+func appendStringIfMissing(slice []string, str string) []string {
+    for _, s := range slice {
+        if s == str {
+            return slice
+        }
+    }
+    return append(slice, str)
+}
