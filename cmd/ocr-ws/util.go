dc1dd8718a57f59aa688e70909f0458c1682f1ce
Jason Jordan <jlj5aj@virginia.edu>
2018-12-18 16:01:34 -0500

    add helper functions to process email delivery of successful/failed requests

--- cmd/ocr-ws/util.go
+++ cmd/ocr-ws/util.go
@@ -7,6 +7,18 @@ import (
 	"os"
 )
 
+type ocrPidInfo struct {
+	pid string // page pid
+	text string
+}
+
+type ocrResultsInfo struct {
+	pid string // request pid
+	details string
+	workDir string
+	pages []ocrPidInfo
+}
+
 func getWorkDir(subDir string) string {
 	return fmt.Sprintf("%s/%s", config.storageDir.value, subDir)
 }
@@ -41,4 +53,54 @@ func appendStringIfMissing(slice []string, str string) []string {
 	}
 
 	return append(slice, str)
+}
+
+func processOcrSuccess(res ocrResultsInfo) {
+	ocrAllText := ""
+	ocrAllFile := fmt.Sprintf("%s/ocr.txt", res.workDir)
+
+	for _, p := range res.pages {
+		// save to one file
+		ocrOneFile := fmt.Sprintf("%s/%s.txt", res.workDir, p.pid)
+		writeFileWithContents(ocrOneFile, p.text)
+
+		ocrAllText = fmt.Sprintf("%s\n\n%s\n", ocrAllText, p.text)
+
+		// post to tracksys
+		//func tsPostText(pid, text string)
+	}
+
+	// save to all file
+	if err := writeFileWithContents(ocrAllFile, ocrAllText); err != nil {
+		logger.Printf("error creating results attachment file: [%s]", err.Error())
+		return
+	}
+
+	emails, err := sqlGetEmails(res.workDir)
+
+	if err != nil {
+		logger.Printf("error retrieving email addresses: [%s]", err.Error())
+		return
+	}
+
+	for _, e := range emails {
+		emailResults(e, fmt.Sprintf("OCR Results for %s", res.pid), "OCR results are attached.", ocrAllFile)
+	}
+
+	os.RemoveAll(res.workDir)
+}
+
+func processOcrFailure(res ocrResultsInfo) {
+	emails, err := sqlGetEmails(res.workDir)
+
+	if err != nil {
+		logger.Printf("error retrieving email addresses: [%s]", err.Error())
+		return
+	}
+
+	for _, e := range emails {
+		emailResults(e, fmt.Sprintf("OCR Failure for %s", res.pid), fmt.Sprintf("OCR failure details: %s", res.details), "")
+	}
+
+	os.RemoveAll(res.workDir)
 }
