71d23dc8d7af8e8212d652fe55b4eb0d7dc18f84
Jason Jordan <jlj5aj@virginia.edu>
2019-01-08 18:19:38 -0500

    implement imagemagick/tesseract logic; upload results to s3

--- cmd/lambda/main.go
+++ cmd/lambda/main.go
@@ -5,8 +5,12 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
+	"io/ioutil"
 	"os"
+	"os/exec"
 	"path"
+	"path/filepath"
+	"strings"
 
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/aws/aws-sdk-go/aws"
@@ -29,12 +33,9 @@ type lambdaResponse struct {
 	Text string `json:"text,omitempty"`
 }
 
-func awsDownloadImage(req lambdaRequest, localFile string) (int64, error) {
-	sess, sessErr := session.NewSession()
-	if sessErr != nil {
-		return -1, errors.New(fmt.Sprintf("Failed to create new session: [%s]", sessErr.Error()))
-	}
+var sess *session.Session
 
+func downloadImage(bucket, key, localFile string) (int64, error) {
 	downloader := s3manager.NewDownloader(sess)
 
 	f, fileErr := os.Create(localFile)
@@ -45,8 +46,8 @@ func awsDownloadImage(req lambdaRequest, localFile string) (int64, error) {
 
 	bytes, dlErr := downloader.Download(f,
 		&s3.GetObjectInput{
-			Bucket: aws.String(req.Bucket),
-			Key:    aws.String(req.Key),
+			Bucket: aws.String(bucket),
+			Key:    aws.String(key),
 		})
 
 	if dlErr != nil {
@@ -56,22 +57,158 @@ func awsDownloadImage(req lambdaRequest, localFile string) (int64, error) {
 	return bytes, nil
 }
 
+func uploadResult(uploader *s3manager.Uploader, bucket, remoteResultsPrefix, resultFile string) error {
+	s3File := path.Join(remoteResultsPrefix, resultFile)
+
+	f, err := os.Open(resultFile)
+	if err != nil {
+		return errors.New(fmt.Sprintf("Failed to open results file: [%s]", err.Error()))
+	}
+	defer f.Close()
+
+	_, err = uploader.Upload(&s3manager.UploadInput{
+		Bucket: aws.String(bucket),
+		Key:    aws.String(s3File),
+		Body:   f,
+	})
+
+	return err
+}
+
+func uploadResults(bucket, remoteResultsPrefix string) error {
+	uploader := s3manager.NewUploader(sess)
+
+	matches, globErr := filepath.Glob("results.*")
+
+	if globErr != nil {
+		return errors.New(fmt.Sprintf("Failed to find results file(s): [%s]", globErr.Error()))
+	}
+
+	for _, resultFile := range matches {
+		if err := uploadResult(uploader, bucket, remoteResultsPrefix, resultFile); err != nil {
+			return errors.New(fmt.Sprintf("Failed to upload result: [%s]", err.Error()))
+		}
+	}
+
+	return nil
+}
+
+func stripExtension(fileName string) string {
+	strippedFileName := strings.TrimSuffix(fileName, filepath.Ext(fileName))
+
+	return strippedFileName
+}
+
+func convertImage(localSourceImage, localConvertedImage string) error {
+	cmd := "magick"
+	args := []string{"convert", "-density", "300", "-units", "PixelsPerInch", "-type", "Grayscale", "+compress", localSourceImage, localConvertedImage}
+
+	if err := exec.Command(cmd, args...).Run(); err != nil {
+		return errors.New(fmt.Sprintf("Failed to convert source image: [%s]", err.Error()))
+	}
+
+	return nil
+}
+
+func ocrImage(localConvertedImage, resultsBase, lang string) error {
+	if lang == "" {
+		lang = "eng"
+	}
+
+	cmd := "tesseract"
+	args := []string{localConvertedImage, resultsBase, "--psm", "1", "-l", lang, "quiet", "txt", "hocr", "pdf"}
+
+	if err := exec.Command(cmd, args...).Run(); err != nil {
+		return errors.New(fmt.Sprintf("Failed to ocr converted image: [%s]", err.Error()))
+	}
+
+	return nil
+}
+
 func handleOcrRequest(ctx context.Context, req lambdaRequest) (string, error) {
-	res := lambdaResponse{}
+	// set file/path variables
 
-	imgFile := fmt.Sprintf("/tmp/%s", path.Base(req.Key))
+	imageBase := path.Base(req.Key)
+	resultsBase := "results"
 
-	bytes, dlErr := awsDownloadImage(req, imgFile)
+	localWorkDir := "/tmp/ocr-ws"
+	localSourceImage := imageBase
+	localConvertedImage := "converted.tif"
+	localResultsTxt := fmt.Sprintf("%s.txt", resultsBase)
+
+	remoteResultsPrefix := path.Join(resultsBase, path.Join(strings.Split(stripExtension(imageBase),"_")...))
+
+	// create aws session
+
+	var sessErr error
+	if sess, sessErr = session.NewSession(); sessErr != nil {
+		return "", errors.New(fmt.Sprintf("Failed to create new session: [%s]", sessErr.Error()))
+	}
+
+	// set needed environment variables
+
+	home := os.Getenv("LAMBDA_TASK_ROOT")
+
+	os.Setenv("LD_LIBRARY_PATH", fmt.Sprintf("%s/lib:%s", home, os.Getenv("LD_LIBRARY_PATH")))
+	os.Setenv("PATH", fmt.Sprintf("%s/bin:%s", home, os.Getenv("PATH")))
+	os.Setenv("TESSDATA_PREFIX", fmt.Sprintf("%s/share/tessdata", home))
+
+	// create and change to temporary working directory
+
+	if err := os.MkdirAll(localWorkDir, 0755); err != nil {
+		return "", errors.New(fmt.Sprintf("Failed to create work dir: [%s]", err.Error()))
+	}
+
+	defer func() {
+		os.Chdir("/")
+		os.RemoveAll(localWorkDir)
+	}()
+
+	if err := os.Chdir(localWorkDir); err != nil {
+		return "", errors.New(fmt.Sprintf("Failed to change to work dir: [%s]", err.Error()))
+	}
+
+	// download image from s3
+
+	_, dlErr := downloadImage(req.Bucket, req.Key, localSourceImage)
 	if dlErr != nil {
 		return "", dlErr
 	}
 
-	//res.Text = fmt.Sprintf("lang: [%s]  bucket: [%s]  key: [%s]  pid: [%s]  title: [%s]  count: [%d]", req.Lang, req.Bucket, req.Key, req.Pid, req.Title, req.Count)
-	res.Text = fmt.Sprintf("imgFile: [%s]  bytes: [%d]\n\nreq: %+v", imgFile, bytes, req)
+	// run magick
+
+	if err := convertImage(localSourceImage, localConvertedImage); err != nil {
+		return "", err
+	}
+
+	// run tesseract
+
+	if err := ocrImage(localConvertedImage, resultsBase, req.Lang); err != nil {
+		return "", err
+	}
+
+	// read ocr text results
+
+	resultsText, readErr := ioutil.ReadFile(localResultsTxt)
+	if readErr != nil {
+		return "", errors.New(fmt.Sprintf("Failed to read ocr results file: [%s]", readErr.Error()))
+	}
+
+	// upload results
+
+	if err := uploadResults(req.Bucket, remoteResultsPrefix); err != nil {
+		return "", err
+	}
+
+	// send response
+
+	res := lambdaResponse{}
+
+	res.Text = string(resultsText)
 
 	output, jsonErr := json.Marshal(res)
 	if jsonErr != nil {
-		return "", errors.New("Failed to serialize output")
+		return "", errors.New(fmt.Sprintf("Failed to serialize output: [%s]", jsonErr.Error()))
 	}
 
 	return string(output), nil
