64699eb66fc0746040ea74b39eb593a36e8a030b
Jason Jordan <jlj5aj@virginia.edu>
2019-01-15 13:52:26 -0500

    add new endpoints; remove unneeded request fields

--- cmd/ocr-ws/generate.go
+++ cmd/ocr-ws/generate.go
@@ -1,19 +1,19 @@
 package main
 
 import (
+	"encoding/json"
+	"errors"
 	"fmt"
 	"net/http"
 	"os"
 	"strconv"
+	"strings"
 
 	"github.com/julienschmidt/httprouter"
 )
 
 type ocrRequest struct {
 	pid   string
-	unit  string
-	pages string
-	token string
 	email string
 	force string
 	lang  string
@@ -22,7 +22,6 @@ type ocrRequest struct {
 type ocrInfo struct {
 	req     ocrRequest // values from original request
 	ts      *tsPidInfo // values looked up in tracksys
-	unitID  int
 	subDir  string
 	workDir string
 	reqID   string
@@ -31,115 +30,60 @@ type ocrInfo struct {
 /**
  * Handle a request for OCR of page images
  */
-func generateHandler(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+func ocrGenerateHandler(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
 	logger.Printf("%s %s", r.Method, r.RequestURI)
 
 	ocr := ocrInfo{}
 
 	// save fields from original request
 	ocr.req.pid = params.ByName("pid")
-	ocr.req.unit = r.URL.Query().Get("unit")
-	ocr.req.pages = r.URL.Query().Get("pages")
-	ocr.req.token = r.URL.Query().Get("token")
 	ocr.req.email = r.URL.Query().Get("email")
 	ocr.req.force = r.URL.Query().Get("force")
 	ocr.req.lang = r.URL.Query().Get("lang")
 
 	// save info generated from the original request
-	ocr.unitID, _ = strconv.Atoi(ocr.req.unit)
 	ocr.subDir = ocr.req.pid
+	ocr.workDir = getWorkDir(ocr.subDir)
 	ocr.reqID = newUUID()
 
-	if ocr.unitID > 0 {
-		// if pages from a specific unit are requested, put them
-		// in a unit subdirectory under the metadata pid
-		ocr.subDir = fmt.Sprintf("%s/%d", ocr.req.pid, ocr.unitID)
-	}
-
-	if len(ocr.req.pages) > 0 {
-		if len(ocr.req.token) == 0 {
-			logger.Printf("Request for partial OCR is missing a token")
-			w.WriteHeader(http.StatusBadRequest)
-			fmt.Fprintf(w, "ERROR: Missing token")
-			return
-		}
-		ocr.subDir = ocr.req.token
-		logger.Printf("Request for partial OCR including pages: %s", ocr.req.pages)
-	}
-
-	// See if destination already exists...
-	ocr.workDir = getWorkDir(ocr.subDir)
-
-	if _, err := os.Stat(sqlFileName(ocr.workDir)); err == nil {
+	// See if tracking database already exists; if so, request is in progress
+	if sqlDatabaseExists(ocr.workDir) == true {
 		// request database already exists; don't start another request, just add email to requestor list
-		logger.Printf("Request already in progress or completed")
+		logger.Printf("Request already in progress; adding email to completion notification list")
 		sqlAddEmail(ocr.workDir, ocr.req.email)
 		fmt.Fprintf(w, "OK")
 		return
 	}
 
-	ts, tsErr := tsGetPidInfo(ocr, w)
+	ts, tsErr := tsGetMetadataPidInfo(ocr)
 
 	if tsErr != nil {
 		logger.Printf("Tracksys API error: [%s]", tsErr.Error())
 		w.WriteHeader(http.StatusNotFound)
-		fmt.Fprintf(w, "ERROR: Could not retrieve PID info")
+		fmt.Fprintf(w, fmt.Sprintf("ERROR: Could not retrieve PID info: [%s]", tsErr.Error()))
 		return
 	}
-
-	// filter out pages with empty pids
-
-	var pages []tsAPICommonFields
-	allTextExists := true
-
-	for _, p := range ts.Pages {
-		if p.Pid == "" {
-			logger.Printf("skipping page with missing pid: %+v", p)
-			continue
-		}
-
-		pages = append(pages, p)
-
-		if p.TextSource == "" {
-			allTextExists = false
-		}
-	}
-
-	ts.Pages = pages
 
 	ocr.ts = ts
 
-	// ensure we have something to process
-
-	if len(ocr.ts.Pages) == 0 {
-		logger.Printf("No pages found")
-		w.WriteHeader(http.StatusNotFound)
-		fmt.Fprintf(w, "ERROR: No pages found for this PID")
-		return
-	}
-
 	// create work dir now (required for adding email to requestor list)
-	os.MkdirAll(ocr.workDir, 0777)
+	os.MkdirAll(ocr.workDir, 0775)
 
 	// check if forcing ocr
 
 	if b, err := strconv.ParseBool(ocr.req.force); err == nil && b == true {
 		if ocr.req.lang != "" {
-			ocr.ts.OcrLanguageHint = ocr.req.lang
+			ocr.ts.Pid.OcrLanguageHint = ocr.req.lang
 		}
-
 		sqlAddEmail(ocr.workDir, ocr.req.email)
-
 		fmt.Fprintf(w, "OK")
-
 		go generateOcr(ocr)
-
 		return
 	}
 
 	// check if ocr/transcription already exists; if so, just email now
 
-	if allTextExists == true {
+	if ocr.ts.Pid.TextSource != "" {
 		logger.Printf("OCR/transcription already exists; emailing now")
 
 		sqlAddEmail(ocr.workDir, ocr.req.email)
@@ -171,26 +115,11 @@ func generateHandler(w http.ResponseWriter, r *http.Request, params httprouter.P
 
 	// check if this is ocr-able
 
-	if ocr.ts.OcrHint != "" && ocr.ts.OcrHint != "Modern Font" && ocr.ts.OcrHint != "Regular Font" {
-		logger.Printf("Cannot OCR: [%s]", ocr.ts.OcrHint)
+	if ocr.ts.isOcrable == false {
+		logger.Printf("Cannot OCR: [%s]", ocr.ts.Pid.OcrHint)
 		w.WriteHeader(http.StatusBadRequest)
 		fmt.Fprintf(w, "ERROR: PID is not in a format conducive to OCR")
 		return
-	}
-
-	// debug info
-	n := len(ocr.ts.Pages)
-	logger.Printf("%d pids:", n)
-	for i, p := range ocr.ts.Pages {
-		txt, txtErr := tsGetText(p.Pid)
-		switch {
-		case txtErr != nil:
-			logger.Printf("[%d/%d] [%s] tsGetText() error: [%s]", i+1, n, p.Pid, txtErr.Error())
-		case txt == "":
-			logger.Printf("[%d/%d] [%s] no text", i+1, n, p.Pid)
-		default:
-			//logger.Printf("[%d/%d] [%s] text:\n\n%s\n\n", i+1, n, p.Pid, txt)
-		}
 	}
 
 	// perform ocr
@@ -200,6 +129,100 @@ func generateHandler(w http.ResponseWriter, r *http.Request, params httprouter.P
 	fmt.Fprintf(w, "OK")
 
 	go generateOcr(ocr)
+}
+
+func getTextForMetadataPid(ts *tsPidInfo) (string, error) {
+	var ocrText strings.Builder
+
+	// preallocate buffer with assumed worst-case of 4K bytes per page
+	ocrText.Grow(len(ts.Pages) * 4096)
+
+	for i, p := range ts.Pages {
+		pageText, txtErr := tsGetText(p.Pid)
+		if txtErr != nil {
+			logger.Printf("[%s] tsGetText() error: [%s]", p.Pid, txtErr.Error())
+			return "", errors.New("ERROR: Could not retrieve page text")
+		}
+
+		fmt.Fprintf(&ocrText, "[Title: %s]\n[Page: %d of %d]\n\n%s\n\n", p.Title, i+1, len(ts.Pages), pageText)
+	}
+
+	return ocrText.String(), nil
+}
+
+func ocrTextHandler(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	logger.Printf("%s %s", r.Method, r.RequestURI)
+
+	ocr := ocrInfo{}
+
+	// save fields from original request
+	ocr.req.pid = params.ByName("pid")
+
+	ts, tsErr := tsGetMetadataPidInfo(ocr)
+
+	if tsErr != nil {
+		logger.Printf("Tracksys API error: [%s]", tsErr.Error())
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprintf(w, fmt.Sprintf("ERROR: Could not retrieve PID info: [%s]", tsErr.Error()))
+		return
+	}
+
+	ocrText, txtErr := getTextForMetadataPid(ts)
+
+	if txtErr != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprintf(w, fmt.Sprintf("ERROR: %s", txtErr.Error()))
+		return
+	}
+
+	fmt.Fprintf(w, "%s", ocrText)
+}
+
+func ocrStatusHandler(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	logger.Printf("%s %s", r.Method, r.RequestURI)
+
+	ocr := ocrInfo{}
+
+	// save fields from original request
+	ocr.req.pid = params.ByName("pid")
+
+	ts, tsErr := tsGetMetadataPidInfo(ocr)
+
+	if tsErr != nil {
+		logger.Printf("Tracksys API error: [%s]", tsErr.Error())
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprintf(w, fmt.Sprintf("ERROR: Could not retrieve PID info: [%s]", tsErr.Error()))
+		return
+	}
+
+	hasOcr, hasTranscription := false, false
+
+	switch ts.Pid.TextSource {
+	case "ocr", "corrected_ocr":
+		hasOcr = true
+	case "transcription":
+		hasTranscription = true
+	}
+
+	status := struct {
+		HasOcr bool `json:"has_ocr"`
+		HasTranscription bool `json:"has_transcription"`
+		IsOcrCandidate bool `json:"is_ocr_candidate"`
+	}{
+		HasOcr: hasOcr,
+		HasTranscription: hasTranscription,
+		IsOcrCandidate: ts.isOcrable,
+	}
+
+	output, jsonErr := json.Marshal(status)
+	if jsonErr != nil {
+		logger.Printf("Failed to serialize output: [%s]", jsonErr.Error())
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprintf(w, fmt.Sprintf("ERROR: Could not serialize PID status: [%s]", jsonErr.Error()))
+		return
+	}
+
+	fmt.Fprintf(w, string(output))
 }
 
 func generateOcr(ocr ocrInfo) {
