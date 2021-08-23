685acbf51912b4631bc1adcda79ffb02bba57198
Lou Foster <louffoster@gmail.com>
2021-08-23 14:09:02 -0400

    fix typo in cmd line param

--- cmd/config.go
+++ cmd/config.go
@@ -68,7 +68,7 @@ func init() {
 	config.concurrentUploads = configStringItem{value: "", configItem: configItem{flag: "o", env: "OCRWS_CONCURRENT_UPLOADS", desc: "concurrent uploads (0 => # cpu cores)"}}
 	config.iiifURLTemplate = configStringItem{value: "", configItem: configItem{flag: "i", env: "OCRWS_IIIF_URL_TEMPLATE", desc: "iiif url template"}}
 	config.tsAPIHost = configStringItem{value: "", configItem: configItem{flag: "h", env: "OCRWS_TRACKSYS_API_HOST", desc: "tracksys host"}}
-	config.tsAPIKey = configStringItem{value: "", configItem: configItem{flag: ";", env: "OCRWS_TRACKSYS_API_KEY", desc: "tracksys write key"}}
+	config.tsAPIKey = configStringItem{value: "", configItem: configItem{flag: "k", env: "OCRWS_TRACKSYS_API_KEY", desc: "tracksys write key"}}
 	config.tsReadOnly = configBoolItem{value: false, configItem: configItem{flag: "r", env: "OCRWS_TRACKSYS_READ_ONLY", desc: "tracksys read-only flag"}}
 	config.emailName = configStringItem{value: "", configItem: configItem{flag: "n", env: "OCRWS_EMAIL_NAME", desc: "email name"}}
 	config.emailAddress = configStringItem{value: "", configItem: configItem{flag: "d", env: "OCRWS_EMAIL_ADDRESS", desc: "email address"}}
